b12e266084c03b5fc9e5be44bbbe975e4873b4a0
Oliwier Kossak <s22018@pjwstk.edu.pl>
2023-06-19 20:06:01 +0200

    removed files

--- hill_climbing_random.h
+++
@@ -1,1 +0,0 @@
-void hill_climbing_random(int iter, int random_number, std::vector<std::vector<int> >& vect_all_combinations, std::vector<std::vector<int> >& vect_all_neighbors_of_subset,  std::vector<int>& arr, int sum_to_find);
--- simulate_annealing.h
+++
@@ -1,1 +0,0 @@
-void simulated_annealing(int iter, int random_number, std::vector<std::vector<int> >& vect_all_combinations, std::vector<std::vector<int> >& vect_all_neighbors_of_subset,  std::vector<int>& arr, int sum_to_find);
